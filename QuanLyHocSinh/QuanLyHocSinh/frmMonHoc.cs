9311176ed646e9729099deaee5bc7d7e8bd0c7da
meomun0 <meomun0@gmail.com>
2018-06-10 23:25:23 +0700

    GH xong 5 chức năng
    Bảng điểm, Kết quả học tập, Lớp học môn học, Thông tin giảng dạy

--- QuanLyHocSinh/QuanLyHocSinh/frmMonHoc.cs
+++ QuanLyHocSinh/QuanLyHocSinh/frmMonHoc.cs
@@ -1,5 +1,5 @@
 ﻿using DAL;
-//using DAO;
+using DAO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -25,7 +25,8 @@ namespace QuanLyHocSinh
                 PhanQuyen(false);
             ShowData();
         }
-
+        DALMonHoc dal_mh = new DALMonHoc();
+        bool check = false;
 
         private void FormatData()
         {
@@ -54,10 +55,11 @@ namespace QuanLyHocSinh
                 btXoa.Enabled = b;
             }
         }
-        
+        string type;
         private void ShowData()
         {
-            
+            dtgvMonHoc.DataSource = dal_mh.GetData();
+            EnabledData(check);
         }
 
         private void btThoat_Click(object sender, EventArgs e)
@@ -69,27 +71,107 @@ namespace QuanLyHocSinh
 
         private void btThem_Click(object sender, EventArgs e)
         {
-            
+            type = "Them";
+            EnabledData(true);
+            EnableMethod(false);
         }
 
         private void btSua_Click(object sender, EventArgs e)
         {
-          
+            type = "Sua";
+            EnabledData(true);
+            EnableMethod(false);
+            txtMaMH.Enabled = false;
         }
 
         private void btOk_Click(object sender, EventArgs e)
         {
-            
+            if (type == "Them")
+            {
+                if (txtMaMH.Text != "" && txtTenMH.Text != "")
+                {
+                    MonHoc mh = new MonHoc(txtMaMH.Text.Trim(), txtTenMH.Text.Trim(), int.Parse(cbHeSo.SelectedItem.ToString()));
+                    if (dal_mh.Them(mh) == true)
+                    {
+                        FormatData();
+                        ShowData();
+                        EnableMethod(true);
+                        MessageBox.Show("Thêm thành công");
+                    }
+                    else
+                    {
+                        Exception ex = dal_mh.GetEx();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                    MessageBox.Show("Bạn cần nhập đủ thông tin phòng ban");
+
+            }
+            else if (type == "Sua")
+            {
+                if (txtMaMH.Text != "" && txtTenMH.Text != "")
+                {
+                    MonHoc mh = new MonHoc(txtMaMH.Text.Trim(), txtTenMH.Text.Trim(), int.Parse(cbHeSo.SelectedItem.ToString()));
+                    if (dal_mh.Sua(mh) == true)
+                    {
+                        FormatData();
+                        ShowData();
+                        MessageBox.Show("Sửa thành công");
+                        EnableMethod(true);
+                    }
+                    else
+                    {
+                        Exception ex = dal_mh.GetEx();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                    MessageBox.Show("Bạn cần nhập đủ thông tin phòng ban");
+            }
+            else if (type == "Xoa")
+            {
+                if (txtMaMH.Text != "" && txtTenMH.Text != "")
+                {
+                    DialogResult dr = MessageBox.Show("Lớp học liên quan đến Bảng điểm và Thông tin giảng dạy, bạn vẫn muốn xóa?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                    if (dr == DialogResult.OK)
+                    {
+                        dal_mh.Xoa(txtMaMH.Text);
+                        EnableMethod(true);
+                        ShowData();
+                    }
+                    else
+                    {
+                        EnabledData(false);
+                        EnableMethod(true);
+                    }
+                }
+            }
         }
 
         private void dtgvMonHoc_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-           
+            try
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row = dtgvMonHoc.Rows[e.RowIndex];
+                txtMaMH.Text = row.Cells[0].Value.ToString().Trim();
+                txtTenMH.Text = row.Cells[1].Value.ToString().Trim();
+                cbHeSo.Text = row.Cells[2].Value.ToString().Trim();
+            }
+            catch (Exception ex)
+            {
+                ex = dal_mh.GetEx();
+                MessageBox.Show(ex.Message);
+            }
         }
-        
+        private int GetHeSo(int heSo)
+        {
+            return heSo;
+        }
         private void cbHeSo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            //GetHeSo(int.Parse(cbHeSo.se))
         }
 
         private void btCancel_Click(object sender, EventArgs e)
@@ -100,17 +182,38 @@ namespace QuanLyHocSinh
 
         private void btXoa_Click(object sender, EventArgs e)
         {
-            
+            type = "Xoa";
+            EnabledData(false);
+            EnableMethod(false);
+            btOk.Visible = true;
+            btCancel.Visible = true;
         }
 
         private void btHienThi_Click(object sender, EventArgs e)
         {
-            
+            btHienThi.Enabled = false;
+            txtTimKiem.Text = "";
+            ShowData();
         }
 
         private void btTiemKiem_Click(object sender, EventArgs e)
         {
-            
+            if (txtTimKiem.Text != "")
+            {
+                btHienThi.Enabled = true;
+                if (dal_mh.GetDataTimKiem(txtTimKiem.Text.Trim()) != null)
+                {
+                    dtgvMonHoc.DataSource = dal_mh.GetDataTimKiem(txtTimKiem.Text.Trim());
+                    MessageBox.Show("Tìm thành công");
+                }
+                else
+                {
+                    Exception ex = dal_mh.GetEx();
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+                MessageBox.Show("Bạn cần nhập thông tin để tìm kiếm !");
         }
     }
 }
--- QuanLyHocSinh/QuanLyHocSinh/frmThongTinGiangDay.cs
+++ QuanLyHocSinh/QuanLyHocSinh/frmThongTinGiangDay.cs
@@ -1,5 +1,5 @@
 ﻿using DAL;
-//using DAO;
+using DAO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -34,8 +34,8 @@ namespace QuanLyHocSinh
             txtMaMH.Text = cbTenMH.SelectedValue.ToString();
             maMH = txtMaMH.Text;
         }
-        
-        
+        DALThongTinGangDay dal_ttgd = new DALThongTinGangDay();
+        bool check = false;
 
         private void FormatData()
         {
@@ -64,37 +64,52 @@ namespace QuanLyHocSinh
                 btXoa.Enabled = b;
             }
         }
-        
+        string type;
         private void ShowData()
         {
-            
+            dtgvThongTinGD.DataSource = dal_ttgd.GetData();
+            EnabledData(check);
         }
         void LoadComGV()
         {
-            
+            cbTenGV.DataSource = dal_ttgd.GetDataComGV();
+            cbTenGV.DisplayMember = "Ten";
+            cbTenGV.ValueMember = "magv";
         }
         void LoadComLH()
         {
-            
+            cbTenLH.DataSource = dal_ttgd.GetDataComLH();
+            cbTenLH.DisplayMember = "Tenlh";
+            cbTenLH.ValueMember = "malh";
         }
         void LoadComMH()
         {
-            
+            cbTenMH.DataSource = dal_ttgd.GetDataComMH();
+            cbTenMH.DisplayMember = "Ten";
+            cbTenMH.ValueMember = "mamh";
         }
 
         private void btThem_Click(object sender, EventArgs e)
         {
-            
+            type = "Them";
+            EnabledData(true);
+            EnableMethod(false);
         }
 
         private void btSua_Click(object sender, EventArgs e)
         {
-            
+            type = "Sua";
+            EnabledData(true);
+            EnableMethod(false);
         }
 
         private void btXoa_Click(object sender, EventArgs e)
         {
-            
+            type = "Xoa";
+            EnabledData(false);
+            EnableMethod(false);
+            btOk.Visible = true;
+            btCancel.Visible = true;
         }
 
         private void btThoat_Click(object sender, EventArgs e)
@@ -106,12 +121,92 @@ namespace QuanLyHocSinh
         string maGV, maLH, maMH;
         private void btOk_Click(object sender, EventArgs e)
         {
-            
+            if (type == "Them")
+            {
+                if (txtThu.Text != "" && txtTiet.Text != "")
+                {
+                    ThongTinGiangDay ttgd = new ThongTinGiangDay(cbTenGV.SelectedValue.ToString(), cbTenLH.SelectedValue.ToString(), txtThu.Text.Trim(), txtTiet.Text.Trim(), cbTenMH.SelectedValue.ToString());
+                    if (dal_ttgd.Them(ttgd) == true)
+                    {
+                        FormatData();
+                        ShowData();
+                        EnableMethod(true);
+                        MessageBox.Show("Thêm thành công");
+                    }
+                    else
+                    {
+                        Exception ex = dal_ttgd.GetEx();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                    MessageBox.Show("Bạn cần nhập đủ thông tin phòng ban");
+
+            }
+            else if (type == "Sua")
+            {
+                if (txtThu.Text != "" && txtTiet.Text != "")
+                {
+                    ThongTinGiangDay ttgd = new ThongTinGiangDay(cbTenGV.SelectedValue.ToString(), cbTenLH.SelectedValue.ToString(), txtThu.Text.Trim(), txtTiet.Text.Trim(), cbTenMH.SelectedValue.ToString());
+                    if (dal_ttgd.Sua(ttgd, maGV, maLH, maMH) == true)
+                    {
+                        FormatData();
+                        ShowData();
+                        MessageBox.Show("Sửa thành công");
+                        EnableMethod(true);
+                    }
+                    else
+                    {
+                        Exception ex = dal_ttgd.GetEx();
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                    MessageBox.Show("Bạn cần nhập đủ thông tin phòng ban");
+            }
+            else if (type == "Xoa")
+            {
+                if (txtThu.Text != "" && txtTiet.Text != "")
+                {
+                    DialogResult dr = MessageBox.Show("Bạn có muốn xóa khồng?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                    if (dr == DialogResult.OK)
+                    {
+                        dal_ttgd.Xoa(txtMaGV.Text, txtMaLH.Text, txtMaMH.Text);
+                        EnableMethod(true);
+                        ShowData();
+                    }
+                    else
+                    {
+                        EnabledData(false);
+                        EnableMethod(true);
+                    }
+                }
+            }
         }
 
         private void dtgvThongTinGD_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            try
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row = dtgvThongTinGD.Rows[e.RowIndex];
+                cbTenGV.Text = row.Cells[1].Value.ToString().Trim();
+                txtMaGV.Text = row.Cells[0].Value.ToString().Trim();
+                maGV = txtMaGV.Text;
+                cbTenLH.Text = row.Cells[3].Value.ToString().Trim();
+                txtMaLH.Text = row.Cells[2].Value.ToString().Trim();
+                maLH = txtMaLH.Text;
+                txtThu.Text = row.Cells[4].Value.ToString().Trim();
+                txtTiet.Text = row.Cells[5].Value.ToString().Trim();
+                txtMaMH.Text = row.Cells[6].Value.ToString().Trim();
+                cbTenMH.Text = row.Cells[7].Value.ToString().Trim();
+                maMH = txtMaMH.Text;
+            }
+            catch (Exception ex)
+            {
+                ex = dal_ttgd.GetEx();
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btThoat_Click_1(object sender, EventArgs e)
@@ -123,7 +218,8 @@ namespace QuanLyHocSinh
 
         private void btCancel_Click(object sender, EventArgs e)
         {
-            
+            EnabledData(false);
+            EnableMethod(true);
         }
 
         private void cbTenGV_SelectedIndexChanged(object sender, EventArgs e)
@@ -133,22 +229,39 @@ namespace QuanLyHocSinh
 
         private void btHienThi_Click(object sender, EventArgs e)
         {
-           
+            btHienThi.Enabled = false;
+            txtTimKiem.Text = "";
+            ShowData();
         }
 
         private void btTiemKiem_Click(object sender, EventArgs e)
         {
-            
+            if (txtTimKiem.Text != "")
+            {
+                btHienThi.Enabled = true;
+                if (dal_ttgd.GetDataTimKiem(txtTimKiem.Text.Trim()) != null)
+                {
+                    dtgvThongTinGD.DataSource = dal_ttgd.GetDataTimKiem(txtTimKiem.Text.Trim());
+                    MessageBox.Show("Tìm thành công");
+                }
+                else
+                {
+                    Exception ex = dal_ttgd.GetEx();
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+                MessageBox.Show("Bạn cần nhập thông tin để tìm kiếm !");
         }
 
         private void cbTenLH_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            txtMaLH.Text = cbTenLH.SelectedValue.ToString();
         }
 
         private void cbTenMH_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
+            txtMaMH.Text = cbTenMH.SelectedValue.ToString();
         }
     }
 }
